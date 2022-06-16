3cc8ec9c73dfa25aa7307308faaad6063fbae0a5
Stefan Kamphuis <stefan@superska.net>
2022-06-16 18:16:05 +0200

    unique app name in DNN

--- client-app/src/App.vue
+++ client-app/src/App.vue
@@ -15,12 +15,12 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    .appEmptyModuleVue3 {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
