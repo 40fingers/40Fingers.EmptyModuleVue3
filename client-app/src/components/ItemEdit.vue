73b047a84cf797b8b20f5c4b812b8a6627c83cb7
Stefan Kamphuis <stefan@superska.net>
2022-06-21 08:24:35 +0200

    playing around with fetching data

--- client-app/src/components/ItemEdit.vue
+++ client-app/src/components/ItemEdit.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="modal" tabindex="-1" id="itemEditModal">
         <div class="modal-dialog">
-            <div class="modal-content" v-if="item">
+            <div class="modal-content" v-if="itemdata.item">
                 <div class="modal-header">
-                    <h5 class="modal-title">{{item.name}}</h5>
+                    <h5 class="modal-title">{{itemdata.item.name}}</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
@@ -11,15 +11,15 @@
                 <div class="modal-body">
                     <div class="form-group">
                         <label for="itemId">Item Id</label>
-                        <input type="text" class="form-control" id="itemId" v-model="item.name" readonly>
+                        <input type="text" class="form-control" id="itemId" v-model="itemdata.item.id" readonly>
                     </div>
                     <div class="form-group">
                         <label for="itemName">Name</label>
-                        <input type="text" class="form-control" id="itemName" v-model="item.name">
+                        <input type="text" class="form-control" id="itemName" v-model="itemdata.item.name">
                     </div>
                     <div class="form-group form-check">
                         <label for="itemDescription">Description</label>
-                        <textarea class="form-control" id="itemDescription" v-model="item.description"></textarea>
+                        <textarea class="form-control" id="itemDescription" v-model="itemdata.item.description"></textarea>
                     </div>
                 </div>
                 <div class="modal-footer">
@@ -28,24 +28,53 @@
                 </div>
             </div>
         </div>
-    </div></template>
+    </div>
+    <div v-if="error">
+        {{itemdata.error}}
+    </div>
+</template>
 
 <script setup>
-    import { defineProps } from 'vue';
-    import { getItem } from "../assets/api";
-
+    import { computed, defineProps } from 'vue';
     //import { ref, onMounted } from 'vue';
+    import { baseUrls, useFetch } from "../assets/api";
+
+    const props = defineProps(["itemId"]);
+    const editItemId = computed(() => props.itemId);
+
+    const url = computed(() => `${baseUrls.getItem}${editItemId.value}`);
+
+    const itemdata = useFetch(url);
+
+    //const { itemId } = toRefs(props);
+    //const initialItem = getItem(props.itemId).item;
+    //const item = ref(initialItem);
+
+    //watch(itemId, (newItemId, prvItemId) => {
+    //    console.log(`change value for item to ${newItemId}`);
+    //    //const newItem = JSON.parse(JSON.stringify(item.value));
+    //    //newItem.id = newItemId;
+    //    const newItem = getItem(newItemId).item;
+    //    console.log(`new item: ${JSON.stringify(newItem.value)}`);
+    //    item.value = newItem.value;
+    //    console.log(`new item value: ${JSON.stringify(item.value)}`);
+    //});
+
+    //const item = computed(() => {
+    //    const newData = getItem(props.itemId).item;
+    //    console.log(`new value for item: ${JSON.stringify(newData)}`)
+    //    return newData;
+    //})
 
     //const data = getDataFromApi();
 
     // https://vuejs.org/api/composition-api-lifecycle.html
     //onMounted(() => {});
 
-    const props = defineProps(["itemId"]);
 
     //const { itemId } = toRefs(props);
 
-    const {item} = getItem(props.itemId);
+    //const {item} = getItem(props.itemId);
 
     //const item = ref(null);
     //watch(itemId, (newItemId, prvItemId) => {
